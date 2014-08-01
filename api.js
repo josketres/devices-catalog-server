963600463c4570cca1c616760f2b8f224970d162
Josue Zarzosa <josketres@gmail.com>
2014-08-01 22:04:29 +0200

    Add borrow functionality

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 // initialize our in-memory database
-var db = [];
+var db = {};
 
 exports.query = function(req, res) {
   var id = req.params.id,
@@ -14,13 +14,54 @@ exports.query = function(req, res) {
 };
 
 exports.register = function(req, res) {
-  var id = db.length, 
-  device = req.body;
-  device.id = id;
-  db.push(device);
-  res.json(db[id]);
+  var device = req.body;
+
+  if(!device.id) {
+    res.status(400).send("Can't register a device without 'id'");
+  } else if(db[device.id]) {
+    res.status(500).send("Device is already registered");
+  } else {
+    device.borrower = undefined;
+    device.status = 'available';
+    db[device.id] = device;
+    res.json(device);
+  }
+};
+
+exports.borrowDevice = function(req, res) {
+  var id = req.params.id,
+    device = db[id],
+    borrower = req.body;
+
+  if(!device) {
+    console.log("No device found for id:" + id);
+    res.status(404).send('Not found');
+  } else if(!borrower['name']) {
+    res.status(400).send("Can't borrow to a borrower without 'name'");
+  } else {
+    device.borrower = borrower;
+    device.borrowedSince = new Date().getTime();
+    device.status = 'borrowed';
+    res.json(device);
+  }
+};
+
+exports.returnDevice = function(req, res) {
+  var id = req.params.id,
+    device = db[id];
+
+  if(!device) {
+    console.log("No device found for id:" + id);
+    res.status(404).send('Not found');
+  } else {
+    device.borrower = undefined;
+    device.borrowedSince = undefined;
+    device.status = 'available';
+    res.json(device);
+  } 
 };
 
 exports.devices = function(req, res) {
+  res.header("Cache-Control", "no-cache");
   res.json(db);
 };
--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.use(express.logger());
 app.get('/api/device/:id', api.query);
 app.post('/api/device', api.register);
 app.get('/api/devices', api.devices);
+app.post('/api/device/:id/borrower', api.borrowDevice);
+app.delete('/api/device/:id/borrower', api.returnDevice);
+
 
 app.listen(8000);
 console.log('Listening on port 8000');
